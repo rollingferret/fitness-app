c6916fc893c9ea1d52a653604aa47910214a2665
Neel <nkachalia1@gmail.com>
2023-11-27 10:46:43 -0500

    create user auth

--- backend/app.js
+++ backend/app.js
@@ -1,6 +1,12 @@
 const express = require("express");
 const cookieParser = require('cookie-parser');
 const logger = require('morgan');
+const debug = require('debug');
+
+const cors = require('cors');
+const csurf = require('csurf');
+const { isProduction } = require('./config/keys');
+const csrfRouter = require('./routes/api/csrf');
 
 const usersRouter = require('./routes/api/users'); // update the import file path
 
@@ -11,7 +17,44 @@ app.use(express.json()); // parse JSON request body
 app.use(express.urlencoded({ extended: false })); // parse urlencoded request body
 app.use(cookieParser()); // parse cookies as an object on req.cookies
 
+app.use(
+  csurf({
+    cookie: {
+      secure: isProduction,
+      sameSite: isProduction && "Lax",
+      httpOnly: true
+    }
+  })
+);
+
+if (!isProduction) {
+    // Enable CORS only in development because React will be on the React
+    // development server (http://localhost:3000). (In production, the Express
+    // server will serve the React files statically.)
+    app.use(cors());
+  }
+
 // Attach Express routers
 app.use('/api/users', usersRouter); // update the path
+app.use('/api/csrf', csrfRouter);
+
+app.use((req, res, next) => {
+    const err = new Error('Not Found');
+    err.statusCode = 404;
+    next(err);
+  });
+
+const serverErrorLogger = debug('backend:error');
+
+app.use((err, req, res, next) => {
+    serverErrorLogger(err);
+    const statusCode = err.statusCode || 500;
+    res.status(statusCode);
+    res.json({
+      message: err.message,
+      statusCode,
+      errors: err.errors
+    })
+  });
 
 module.exports = app;
