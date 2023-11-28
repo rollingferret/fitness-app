9ae947d14f3b73db3af0e46b84a926bedb27e96c
Naoko Sugawara <infoseek122@gmail.com>
2023-11-28 11:06:24 -0500

    added ui state

---
+++ frontend/src/store/ui.js
@@ -0,0 +1,31 @@
+
+const OPEN_MODAL = "OPEN_MODAL";
+const CLOSE_MODAL = "CLOSE_MODAL";
+
+export const openModal = (modalType) => ({
+    type: OPEN_MODAL,
+    payload: modalType
+});
+
+export const closeModal = () => ({
+    type: CLOSE_MODAL 
+});
+
+const initialState = { 
+    modal: false
+};
+
+const uiReducer = (state = initialState, action) => {
+    const newState = { ...state };
+
+    switch (action.type) {
+        case OPEN_MODAL:
+            newState.modal = action.payload;
+            return newState;
+        case CLOSE_MODAL:
+            newState.modal = false;
+            return newState;
+        default:
+            return state;
+    }
+};
