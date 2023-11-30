ae4a9190354856bbe3a48fc62c1f950145a0a8f8
Neel <nkachalia1@gmail.com>
2023-11-30 14:54:01 -0500

    css

--- frontend/src/components/Runs/RunBox.js
+++ frontend/src/components/Runs/RunBox.js
@@ -1,12 +1,11 @@
 import { Link } from 'react-router-dom';
-import {useSelector } from 'react-redux';
-import "./RunBox.css"
+import { useSelector } from 'react-redux';
 import { useDispatch } from 'react-redux';
 import { deleteRun } from '../../store/runs';
+import "./RunBox.css";
 
 function RunBox({ run }) {
-  const { distance, hours, minutes, seconds, author, _id: runId } = run;
-  // const { text, author,  } = run;
+  const { distance, hours, minutes, seconds, author, _id: runId, createdAt } = run;
   const currentUser = useSelector(state => state.session.user);
   const dispatch = useDispatch();
 
@@ -14,19 +13,66 @@ function RunBox({ run }) {
     dispatch(deleteRun(runId));
   };
 
-  const formatRunData = () => {
-    return `${distance} km, ${hours}h ${minutes}m ${seconds}s`;
+  const formatRunDistance = () => {
+    return (
+      <p style={{ color: '#000', fontSize: '24px' }}>
+        <span style={{ fontSize: '36px', color: 'green' }}>{distance}</span> <span style={{ fontSize: '36px', color: 'green' }}>miles</span>
+      </p>
+    );
+  };
+
+  const calculatePace = () => {
+    const totalMinutes = hours * 60 + minutes + seconds / 60;
+    const pace = totalMinutes / distance;
+
+    const paceMinutes = Math.floor(pace);
+    const paceSeconds = Math.round((pace - paceMinutes) * 60);
+
+    return (
+      <p style={{ color: '#000', fontSize: '18px' }}>
+        Pace: {paceMinutes}:{paceSeconds < 10 ? `0${paceSeconds}` : paceSeconds} <span style={{ fontSize: 'medium' }}>min/mile</span>
+      </p>
+    );
+  };
+
+  const formatRunTime = () => {
+    const formattedHours = hours < 10 ? `0${hours}` : hours;
+    const formattedMinutes = minutes < 10 ? `0${minutes}` : minutes;
+    const formattedSeconds = seconds < 10 ? `0${seconds}` : seconds;
+
+    return (
+      <p style={{ color: '#000', fontSize: '18px' }}>
+        Time: {formattedHours}:{formattedMinutes}:{formattedSeconds}
+      </p>
+    );
+  };
+
+  const formatCreatedAt = () => {
+    const createdDate = new Date(createdAt);
+    const options = { year: 'numeric', month: 'numeric', day: 'numeric' };
+    return (
+      <p style={{ fontWeight: 'bold', color: '#000' }}>
+        {createdDate.toLocaleDateString(undefined, options)}
+      </p>
+    );
   };
 
   return (
     <div className="run">
-      <h3>{author.username}</h3>
-      <p>{formatRunData()}</p>
+      {/* <h3>{author.username}</h3> */}
+      <p>{formatCreatedAt()}</p>
+      <p>{formatRunDistance()}</p>
+      <p>{formatRunTime()}</p>
+      <p>{calculatePace()}</p>
       {currentUser && currentUser._id === author._id && (
-        <>
+        <div className="button-container">
+        <button className="edit-button">
           <Link to={`/runs/update/${runId}`}>Edit</Link>
-          <button onClick={handleDelete}>Delete</button>
-        </>
+        </button>
+        <button onClick={handleDelete} className="delete-button">
+          Delete
+        </button>
+      </div>
       )}
     </div>
   );
