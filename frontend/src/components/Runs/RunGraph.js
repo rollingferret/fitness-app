d1eee177a273ddbb2e40912ee3f56a426349fe2a
Naoko Sugawara <infoseek122@gmail.com>
2023-12-02 06:10:47 -0500

    fixed logout

--- frontend/src/components/Runs/RunGraph.js
+++ frontend/src/components/Runs/RunGraph.js
@@ -1,48 +1,71 @@
 import React, { useEffect, useRef } from 'react';
-// import * as d3 from 'd3';
+import * as d3 from 'd3';
 
-function RunGraph({ runningData }) {
+function RunGraph({ runs }) {
   const svgRef = useRef();
 
-  // useEffect(() => {
-  //   const margin = { top: 20, right: 20, bottom: 30, left: 50 };
-  //   const width = 600 - margin.left - margin.right;
-  //   const height = 400 - margin.top - margin.bottom;
+  useEffect(() => {
+    if (!Array.isArray(runs) || runs.length === 0) {
+      return; // Exit if runs is not an array or is empty
+    }
 
-  //   const svg = d3.select(svgRef.current)
-  //     .attr('width', width + margin.left + margin.right)
-  //     .attr('height', height + margin.top + margin.bottom)
-  //     .append('g')
-  //     .attr('transform', 'translate(' + margin.left + ',' + margin.top + ')');
+    const margin = { top: 20, right: 20, bottom: 60, left: 60 }; // Adjusted margins for labels
+    const width = 600 - margin.left - margin.right;
+    const height = 400 - margin.top - margin.bottom;
 
-  //   const xScale = d3.scaleLinear()
-  //     .domain([0, runningData.length])
-  //     .range([0, width]);
+    // Clear existing SVG to avoid duplications
+    d3.select(svgRef.current).selectAll("*").remove();
 
-  //   const yScale = d3.scaleLinear()
-  //     .domain([0, d3.max(runningData, d => d.time)])
-  //     .range([height, 0]);
+    const svg = d3.select(svgRef.current)
+      .attr('width', width + margin.left + margin.right)
+      .attr('height', height + margin.top + margin.bottom)
+      .append('g')
+      .attr('transform', `translate(${margin.left},${margin.top})`);
 
-  //   const line = d3.line()
-  //     .x((d, i) => xScale(i))
-  //     .y(d => yScale(d.time));
+    const xScale = d3.scaleTime()
+      .domain(d3.extent(runs, d => new Date(d.createdAt)))
+      .range([0, width]);
 
-  //   svg.append('path')
-  //     .datum(runningData)
-  //     .attr('class', 'line')
-  //     .attr('d', line)
-  //     .attr('fill', 'none')
-  //     .attr('stroke', 'steelblue');
+    const yScale = d3.scaleLinear()
+      .domain([0, d3.max(runs, d => d.distance)])
+      .range([height, 0]);
 
-  //   svg.append('g')
-  //     .attr('transform', 'translate(0,' + height + ')')
-  //     .call(d3.axisBottom(xScale));
+    const line = d3.line()
+      .x(d => xScale(new Date(d.createdAt)))
+      .y(d => yScale(d.distance));
 
-  //   svg.append('g')
-  //     .call(d3.axisLeft(yScale));
-  // }, [runningData]);
+    svg.append('path')
+      .datum(runs)
+      .attr('d', line)
+      .attr('fill', 'none')
+      .attr('stroke', 'steelblue');
 
-  return <svg ref={svgRef} />;
+    // X-axis
+    svg.append('g')
+      .attr('transform', `translate(0,${height})`)
+      .call(d3.axisBottom(xScale));
+
+    // Y-axis
+    svg.append('g')
+      .call(d3.axisLeft(yScale));
+
+    // X-axis label
+    svg.append('text')
+      .attr('text-anchor', 'end')
+      .attr('x', width)
+      .attr('y', height + margin.bottom - 10)
+      .text('Date');
+
+    // Y-axis label
+    svg.append('text')
+      .attr('text-anchor', 'end')
+      .attr('transform', 'rotate(-90)')
+      .attr('y', -margin.left + 20)
+      .attr('x', -margin.top)
+      .text('Distance (km)');
+  }, [runs]);
+
+  return <svg ref={svgRef}></svg>;
 }
 
 export default RunGraph;
